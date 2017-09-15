069d8d5d390679aba672978950580df85ac75963
viktor.reinok <viktor.reinok@ipfdigital.com>
2017-09-15 23:41:35 +0300

    Add javadoc

--- collector/src/main/java/com/concretepage/MyApplication.java
+++ collector/src/main/java/com/concretepage/MyApplication.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ *
+ */
 @SpringBootApplication
 @ComponentScan({"com.concretepage"})
 @EntityScan("com.concretepage.entity")
